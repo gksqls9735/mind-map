a1a767ebdb07568e8f22eefd43fc5908f1192c96
서한빈 <gksqls9735@gmail.com>
2025-09-22 11:19:45 +0900

    Feat: Configure dependencies for Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
-
   return (
-    <>
-      ELECTRON SETTING
-    </>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        TailWind 적용
+      </h1>
+    </div>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,27 @@ import react from '@vitejs/plugin-react'
 import electron from 'vite-plugin-electron/simple'
 import { fileURLToPath } from 'node:url'
 
-// __dirname을 ES Module 방식으로 정의
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react(),
-  electron({
-    main: {
-      entry: 'electron/main.ts' // Electron의 메인 프로세스 파일
-    },
-    preload: {
-      input: path.join(__dirname, 'electron/preload.ts')  // Preload 스크립트 파일
-    },
-    renderer: {},// 렌더러 프로세스에 Node.js API를 사용할 수 있게 설정
-  })
+  plugins: [
+    react(),
+    electron({
+      main: {
+        entry: 'electron/main.ts'
+      },
+      preload: {
+        input: path.join(__dirname, 'electron/preload.ts')
+      },
+      renderer: {},
+    })
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     }
+  },
+  css: {
+    postcss: './postcss.config.js',
   }
 })
